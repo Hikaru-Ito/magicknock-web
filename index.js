781716fd6b81cafe7b33a4bcd7673487e00e30d5
Hikaru <hikaru.ito.1995@gmail.com>
2017-08-03 00:23:42 +0900

    update logo

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var layouts     = require('metalsmith-layouts')
 var permalinks  = require('metalsmith-permalinks')
 
 Metalsmith(__dirname)
-  .metadata({
-    title: "MagicKnock",
-    sitename: "MagicKnock",
-    description: "魔法のように、あらゆるものをノックでコントロールできる実世界指向インタフェース",
-    generator: "Metalsmith",
-    url: "http://magicknock.com/"
-  })
-  .source('./src')
-  .destination('./build')
-  .clean(false)
-  .use(markdown())
-  .use(permalinks())
-  .use(layouts({
-    engine: 'handlebars'
-  }))
-  .build(function(err, files) {
-    if (err) { throw err; }
-  });
+    .metadata({
+        title: "MagicKnock",
+        sitename: "MagicKnock",
+        description: "魔法のように、あらゆるものをノックでコントロールできる実世界指向インタフェース",
+        generator: "Metalsmith",
+        url: "http://magicknock.com/"
+    })
+    .source('./src')
+    .destination('./build')
+    .clean(false)
+    .use(markdown())
+    .use(permalinks())
+    .use(layouts({
+        engine: 'handlebars'
+    }))
+    .build(function(err, files) {
+        if (err) { throw err; }
+    });
